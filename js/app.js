456630b1247c35dcb6a5cc51f792b0542176b375
Mike McCarty <mike.p.mccarty@gmail.com>
2022-10-18 13:30:21 -0700

    Able to get results and chart to appear upon maxing out votes

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ let votes = document.querySelector('#resultsViews');
 let viewResults = document.querySelector('#viewResults');
 console.log(image1);
 console.log(votes);
-let maxNumberOfVotes = 25;
+let maxNumberOfVotes = 5;
 let numberUserVotes = 0;
 
 // Global Functions
@@ -115,12 +115,72 @@ function handleClicks(event) {
     if (numberUserVotes === maxNumberOfVotes) {
         myProduct.removeEventListener('click', handleClicks);
         console.log('myProduct is off');
-        viewResults.className = 'clicks-allowed';
-        viewResults.addEventListener('click', displayResults);
-        console.log('viewResults is on');
+        // Removed for lab 12
+        // viewResults.className = 'clicks-allowed';
+        // viewResults.addEventListener('click', displayResults);
+        // console.log('viewResults is on');
+        displayResults();
+        chartProducer();
     } else {
         displayProducts();
     }
+}
+
+
+function chartProducer() {
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+      ];
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+      }]
+    };
+
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        },
+      };
+
+      const myChart = new Chart(
+        document.getElementById('chart'),
+        config
+      );
+
 }
 
 // Step 2: TheEvent Listener
