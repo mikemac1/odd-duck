6bb73a477cecd5542eca4b10b1fd37d5d641bf2e
Mike McCarty <mike.p.mccarty@gmail.com>
2022-10-17 20:02:16 -0700

    Made several changes to get results to display first without button then with

--- js/app.js
+++ js/app.js
@@ -4,15 +4,24 @@
 
 console.log('Please say Hi!');
 
-// Global Variables & Functions
-// Expect 19 products to load
-productArray = []
+// Global Variables
+// Step 1: Window Into The Dom
+let myProduct = document.querySelector('#oddDucks');
+let image1 = document.querySelector('#duckOne');
+let image2 = document.querySelector('#duckTwo');
+let image3 = document.querySelector('#duckThree');
+let votes = document.querySelector('#resultsViews');
+let viewResults = document.querySelector('#viewResults');
+console.log(image1);
+console.log(votes);
+let maxNumberOfVotes = 25;
+let numberUserVotes = 0;
 
-function selectRandomProduct() {
+// Global Functions
+function randomProduct() {
     // // return Math.floor(math.random() * (max - min) + min);
-    return Math.floor(math.random() * (productArray.length - 0) + 0);
+    return Math.floor(Math.random() * (productArray.length));
 }
-
 
 // Generic Product Constructor Object
 function Product(name, fileExtension) {
@@ -23,7 +32,7 @@ function Product(name, fileExtension) {
     this.shown = 0;
 }
 
-// Products To Be Added WITH "let variable = new Goat('file name', 'fileExtension);
+// Products To Be Added
 let bag = new Product('bag', 'jpg');
 let banana = new Product('banana', 'jpg');
 let bathroom = new Product('bathroom', 'jpg');
@@ -33,7 +42,7 @@ let bubblegum = new Product('bubblegum', 'jpg');
 let chair = new Product('chair', 'jpg');
 let cthulhu = new Product('cthulhu', 'jpg');
 let dogDuck = new Product('dog-duck', 'jpg');
-let dragon = new Product('dragoon', 'jpg');
+let dragon = new Product('dragon', 'jpg');
 let pen = new Product('pen', 'jpg');
 let petSweep = new Product('pet-sweep', 'jpg');
 let scissors = new Product('scissors', 'jpg');
@@ -44,166 +53,78 @@ let unicorn = new Product('unicorn', 'jpg');
 let waterCan = new Product('water-can', 'jpg');
 let wineGlass = new Product('wine-glass', 'jpg');
 
+let productArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass];
+console.log(productArray.length);
 
 
+// User is presented a trio of products
+function displayProducts() {
+    let product1 = randomProduct();
+    let product2 = randomProduct();
+    let product3 = randomProduct();
+    console.log(product1, product2, product3);
+    // seriously consider using an array --->how do you find out if an array includes something
+    while (product1 === product2 || product2 === product3) {
+        product2 = randomProduct();
+    }
+    while (product1 === product3) {
+        product1 = randomProduct();
+    }
+    image1.src = productArray[product1].src;
+    image1.alt = productArray[product1].name;
+    productArray[product1].shown++;
+    console.log(productArray[product1].shown);
+    image2.src = productArray[product2].src;
+    image2.alt = productArray[product2].name;
+    productArray[product2].shown++;
+    console.log(productArray[product2].shown);
+    image3.src = productArray[product3].src;
+    image3.alt = productArray[product3].name;
+    productArray[product3].shown++;
+    console.log(productArray[product3].shown);
+}
 
+// Display & Tally Voting Results
+function displayResults() {
+    // document.getElementById("resultsViews").innerHTML = "";
+    for (let i = 0; i < productArray.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = `${productArray[i].name}: ${productArray[i].votes} votes`;
+        votes.appendChild(li);
+    }
+}
 
-// A Collection of Products
+// // Step 3: The Function
+function handleClicks(event) {
 
-// User is presented a trio of products ---> 3 products at a time
+    if (event.target === myProduct) {
+        alert("Please click on a product image.");
+    }
+    console.log('click');
+    console.log(event.target.alt); //specifically targets the product clicked using its name
+    numberUserVotes++;
+    let clickedProduct = event.target.alt;
 
-// User should see 3 product photos
+    for (let i = 0; i < productArray.length; i++) {
+        if (clickedProduct === productArray[i].name) {
+            console.log(productArray[i]);
+            productArray[i].votes++;
+            break;
+        }
+    }
+    if (numberUserVotes === maxNumberOfVotes) {
+        myProduct.removeEventListener('click', handleClicks);
+        console.log('myProduct is off');
+        viewResults.className = 'clicks-allowed';
+        viewResults.addEventListener('click', displayResults);
+        console.log('viewResults is on');
+    } else {
+        displayProducts();
+    }
+}
 
-// User vote on their favorite product by clicking on it
+// Step 2: TheEvent Listener
+// when addeventlistener handles the click it sends all information from the click
+myProduct.addEventListener('click', handleClicks);
 
-// Data collection will show live updates to the number of votes tallied for each product
-
-// Global variables
-// - Score for the favorite
-// - Count the number of views a product receives
-// - Count the images
-// - Windows into the DOM
-// - Photo Array of product instances example - goat[randomNumner].src
-
-// Goat Constructor
-// - goat image (src)
-// - goat score (how many times has this goat been voted on)
-// - goat photo alt text
-// - goat views - how many times did the user see the goat
-
-// Goat Methods
-// - Change out the goat images, increment the goat views
-// - generate a random number, pass it into the goat array
-// - check to make sure the goat images are different
-// - event listener (click) - tally the vote for the goat
-
-// create all the instances of goat
-// - 
-// AFTER 15 ROUNDS OF VOTING...?
-// - Give the user the results (views and the score)
-// - Stop voting
-// - Stop generating new goat photos
-
-
-// After the game gets to 15 rounds of voting
-// original section + div {
-// pointer-events: none;
-
-// Style the button to allow "View Results" to be clicked
-// section + div.clicks-allowes {
-// background-color: color;
-// color: white;
-// cursor: pointer;
-// pointer-events: auto;
-
-
-
-
-
-
-
-
-// let myContainer = document.querySelector('section');
-// let results = document.querySelector('ul');
-// let resultBtn = document.querySelector('button');
-
-// let myImage1 = document.querySelector('section img:first-child'};
-// let myImage2 = document.querySelector('section img:nth-child(2)');
-
-// let howManyTimesUserHasVoted = 0;
-// let maxNumberOfVotes = 15;
-
-// console.log(myImage2);
-
-// image2.src = 'images/sassy-goat.jpg';
-
-// function Goat (name, fileExtension = 'jpg') {
-// 	this.name = name;
-// 	this.fileExtension = fileExtension;
-// 	this.src = `images/${this.name}.${this.fileExtension}`;
-// 	this.votes = 0;
-// 	this.views = 0;
-// }
-
-// //LIST OF GOATS WITH "let variable = new Goat('file name', 'fileExtension);
-
-
-// //CREATE A GOAT ARRAY MANUALLY "let allGoats = [];
-
-// //HOW TO CHANGE THE GOATS AUTOMATICALLY BY USING A RANDOM NUMBER
-
-// function selectRandomGoat() {
-// // random number between 0 and the length of the array(7)
-// // not inclusive as we don't want it to = 7
-// // return Math.floor(math.random() * (max - min) + min);
-// return Math.floor(math.random() * (allGoats.length - 0) + 0);
-// }
-
-// function renderGoats() {
-// 	let goat1 = selectRandomGoat();
-// 	let goat2 = selectRandomGoat();
-// 	console.log(goat1, goat2);
-// 	// seriously consider using an array --->how do you find out if an array includes something
-// 	while (goat1 === goat2) {
-// 	goat2 = selectRandomGoat();	
-// }
-
-// 	image1.src = allGoats[goat1].src;
-// 	image1.alt = allGoats[goat1].name;
-// 	allGoats[goat1].view++;
-// 	image2.src = allGoats[goat2].src;
-// 	image2.alt = allGoats[goat2].name;
-// 	allGoats[goat2].view++;
-// }
-// // Step 2 for Event Listener
-// myContainer.addEventListener('click', handleGoatClicks); // when addeventlistener handles the click it sends all information from the click via any keyword used (i.e. event, click, whatever the developer wants)
-
-
-// function renderResults() {
-// 	// create a listing of each results
-// 	for (let i=0; i < allGoats.length; i++) {
-// 	let li = doument.createElement('li);
-// 	li.textContent = `${allGoats[i].name} had ${allGoats[i].views} views and ${allGoats[i].score} votes`;	
-// results.appendChild(li);
-// 	}
-// }
-
-
-
-
-
-// // Step 3 Function
-
-// function handleGoatClicks(event) {
-
-// if (event.target === myContainer) {
-// alert("Please click on an image.")
-// }
-// console.log('click');
-// console.log(event.target.alt); //specifically targets the goat clicked using its name
-// howManyTimesUserHasVoted++;
-// let clickedGoat = event.target.alt;
-
-// for(let i = 0; i < allGoats.length; i++) {
-// if (event.target.alt === allGoats[i].name) {
-// 	console.log(allGoats[i]);
-// 	allGoats[i].score++;
-// 	break;
-// 	}
-// 	}
-// if (howManyTimesUserHasVoted === maxNumberofVotes) {
-// 	//STOP GAME
-// 	myContainer.removeEventListener('click', handleClick);
-// 	resultsBtn.addEventListener(
-// } else {
-// renderGoats();
-// }
-// }
-
-
-
-// renderGoats();
-
-
-// image2.src = AllGoats[0].src
-
+displayProducts();
