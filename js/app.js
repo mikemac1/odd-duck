6443670f88871d074d0e84fa15a567856e87d84e
Mike McCarty <mike.p.mccarty@gmail.com>
2022-10-18 13:42:34 -0700

    Successfully added voting and displayed data to chart

--- js/app.js
+++ js/app.js
@@ -128,58 +128,60 @@ function handleClicks(event) {
 
 
 function chartProducer() {
-    const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-      ];
+
+    let productNames = [];
+    let productShown = [];
+    let productVotes = [];
+    for (let i = 0; i < productArray.length; i++) {
+        productNames.push(productArray[i].name);
+        productShown.push(productArray[i].shown);
+        productVotes.push(productArray[i].votes);
+    }
+
+    const labels = productNames;
     const data = {
-      labels: labels,
-      datasets: [{
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 205, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(201, 203, 207, 0.2)'
-        ],
-        borderColor: [
-          'rgb(255, 99, 132)',
-          'rgb(255, 159, 64)',
-          'rgb(255, 205, 86)',
-          'rgb(75, 192, 192)',
-          'rgb(54, 162, 235)',
-          'rgb(153, 102, 255)',
-          'rgb(201, 203, 207)'
-        ],
-        borderWidth: 1
-      }]
+        labels: labels,
+        datasets: [
+            {
+                label: 'Number Of Votes',
+                data: productVotes,
+                backgroundColor: [
+                    'rgba(151, 51, 201, 0.2)',
+                ],
+                borderColor: [
+                    'rgb(151, 51, 201)',
+                ],
+                borderWidth: 1
+            },
+            {
+                label: 'Number Of Times Shown',
+                data: productShown,
+                backgroundColor: [
+                    'rgba(11, 192, 47, 0.2)',
+                ],
+                borderColor: [
+                    'rgb(11, 192, 47)',
+                ],
+                borderWidth: 1
+            }]
     };
 
     const config = {
         type: 'bar',
         data: data,
         options: {
-          scales: {
-            y: {
-              beginAtZero: true
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
-          }
         },
-      };
+    };
 
-      const myChart = new Chart(
+    const myChart = new Chart(
         document.getElementById('chart'),
         config
-      );
+    );
 
 }
 
